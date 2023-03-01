e5301fd5aeef2032acb0325735f3dd1e87791528
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-03-01 23:22:26 +0900

    add:サブテストの追加

--- hello_test.go
+++ hello_test.go
@@ -6,17 +6,17 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
-		
+
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T{
-		got := Hello()
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
 		want := "Hello, World"
 
-		if got != nil {
+		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
-	}))
+	})
 }
