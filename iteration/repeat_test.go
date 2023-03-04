99374d2f1059a53f32b2f0b5dc602d1445dae832
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-03-05 08:46:18 +0900

    add:練習問題の実装

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,10 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a")
@@ -9,6 +13,21 @@ func TestRepeat(t *testing.T) {
 	if repeated != expected {
 		t.Errorf("expected %s but got %s", expected, repeated)
 	}
+}
+
+func TestCompare(t *testing.T) {
+	compared := strings.Compare("aaa", "aaa")
+	expected := 0
+
+	if compared != expected {
+		t.Errorf("expected %v but got %v", expected, compared)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("b")
+	fmt.Println(repeated)
+	// output: bbbbb
 }
 
 func BenchmarkRepeat(b *testing.B) {
