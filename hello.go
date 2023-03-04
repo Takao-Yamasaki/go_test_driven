797c4051b07cc6149aa6883c42f52afac95cfe8e
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-03-04 21:08:57 +0900

    modify:リファクタリング

--- hello.go
+++ hello.go
@@ -11,17 +11,20 @@ const french = "French"
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
-
 	}
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case spanish:
 		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return prefix + name
+	return
 }
 
 func main() {
