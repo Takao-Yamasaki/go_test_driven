462fb161d6dc33ef8d48de280e7ecfc1038e9836
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-03-05 19:22:11 +0900

    add:Triangeメソッドの実装

--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -19,19 +19,18 @@ type Shape interface {
 
 // 長方形の面積を計算する
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+	areaTest := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+	for _, tt := range areaTest {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
 		}
 	}
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
 }
