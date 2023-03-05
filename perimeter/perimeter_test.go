fa3f62ae15ef401e92c23fdc57705b455e180aa3
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-03-05 16:05:25 +0900

    add:インターフェースの追加

--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -13,13 +13,25 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
+type Shape interface {
+	Area() float64
+}
+
 // 長方形の面積を計算する
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	}
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
+	})
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
 }
