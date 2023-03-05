c1a43069f5a803a987b50033f050038404768383
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-03-05 15:03:20 +0900

    add:構造体の追加

---
+++ perimeter/perimeter_test.go
@@ -0,0 +1,25 @@
+package perimeter
+
+import "testing"
+
+// 長方形の周囲を計算する
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+// 長方形の面積を計算する
+func TestArea(t *testing.T) {
+	rectangle := Rectangle{12.0, 6.0}
+	got := Area(rectangle)
+	want := 72.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
